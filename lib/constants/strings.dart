7904b207939aea82a080eadb2f72104d5d722830
ThePGCoder <108600974+ThePGCoder@users.noreply.github.com>
2023-01-04 08:04:23 +1300

    Update strings.dart

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -1,3 +1,3 @@
-const String apiKey = "e9901cbd9a7ce3695c195845242d1496";
+const String apiKey = "YourApiKey";
 const String currentEndpoint = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
 const String daysEndpoint = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
